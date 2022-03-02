a4c3582bd2f481c3681dd2403e7a58d47cae33c9
James Taylor <james.taylor_rw@outlook.com>
2022-03-03 00:21:38 +0800

    ...

---
+++ include/Tlpp/Test/Error.hpp
@@ -0,0 +1,22 @@
+﻿#ifndef TLPP_ERROR_HPP
+#define TLPP_ERROR_HPP
+
+class Error
+{
+public:
+	Error() = default;
+
+	Error(const wchar_t* message_) noexcept
+		: message(message_)
+	{}
+
+	[[nodiscard]] const wchar_t* what() const noexcept
+	{
+		return message;
+	}
+
+private:
+	const wchar_t* message = nullptr;
+};
+
+#endif // TLPP_ERROR_HPP
---
+++ include/Tlpp/Test/Test.hpp
@@ -0,0 +1,45 @@
+﻿#ifndef TLPP_TEST_HPP
+#define TLPP_TEST_HPP
+
+#include "../Macro.h"
+#include "Error.hpp"
+#include <stdio.h>
+// TODO:TEST_ASSERT & TEST_CHECK that throw Error
+namespace tl
+{
+	namespace test
+	{
+		class Test
+		{
+		public:
+			Test() = delete;
+
+			using TestFuncType = void (*)();
+
+			static void RunAndDispose(int argc, char* argv[])
+			{}
+			static void RegisterTestCase(TestFuncType func)
+			{}
+		};
+
+
+#define TEST(NAME)                                                                  \
+	void CONCAT(test_case_method_, NAME)();                                         \
+	struct CONCAT(test_case_class_, NAME)                                           \
+	{                                                                               \
+		CONCAT(test_case_class_, NAME)()                                            \
+		{                                                                           \
+			::tl::test::Test::RegisterTestCase(&CONCAT(test_case_method_, NAME));   \
+			printf_s("----------- TEST ");                                          \
+			printf_s(QUOTE(NAME));                                                  \
+			printf_s(" -----------\n");                                             \
+			CONCAT(test_case_method_, NAME)();                                      \
+			printf_s("\n");                                                         \
+		}                                                                           \
+	} CONCAT(test_case_object_, NAME);                                              \
+	void CONCAT(test_case_method_, NAME)()
+
+	} // namespace test
+} // namespace tl
+
+#endif // TLPP_TEST_HPP
