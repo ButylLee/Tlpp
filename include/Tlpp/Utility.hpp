6b61d214ee36cb1b4f76c0fc6ea0b75e6c833e01
James Taylor <james.taylor_rw@outlook.com>
2022-03-04 22:27:18 +0800

    ...

--- include/Tlpp/Utility.hpp
+++ include/Tlpp/Utility.hpp
@@ -2,6 +2,7 @@
 #define TLPP_UTILITY_HPP
 
 #include <Tlpp/Utility/AddressOf.hpp>
+#include <Tlpp/Utility/AsConst.hpp>
 #include <Tlpp/Utility/ForwardValue.hpp>
 #include <Tlpp/Utility/MoveValue.hpp>
 
---
+++ include/Tlpp/Utility/AsConst.hpp
@@ -0,0 +1,22 @@
+﻿#ifndef TLPP_UTILITY_AS_CONST_HPP
+#define TLPP_UTILITY_AS_CONST_HPP
+
+#include <Tlpp/TypeTraits/cv_traits.hpp>
+
+namespace tl
+{
+	namespace utility
+	{
+		template<typename T>
+		constexpr type_traits::add_const_t<T>& AsConst(T& val) noexcept
+		{
+			return val;
+		}
+
+		template<typename T>
+		void AsConst(const T&&) = delete;
+
+	} // namespace utility
+} // namespace tl
+
+#endif // TLPP_UTILITY_AS_CONST_HPP
