ee303c0ff3751296abfe40a2fb8103813cb1a68e
James Taylor <james.taylor_rw@outlook.com>
2022-03-07 14:42:35 +0800

    ...

--- TODO.md
+++ TODO.md
@@ -1,8 +1,9 @@
 # TODO
 
-- impl nothrow traits - swappable
+- impl nothrow traits - swappable, nothrow constructible alternative
 - impl type_traits
 - impl Console
 - impl Test
 - impl string
 - complete CMakeLists.txt
+- add comments for all codes
--- include/Tlpp/TypeTraits/type_relationships.hpp
+++ include/Tlpp/TypeTraits/type_relationships.hpp
@@ -1,7 +1,9 @@
 ﻿#ifndef TLPP_TYPE_TRAITS_TYPE_RELATIONSHIPS_HPP
 #define TLPP_TYPE_TRAITS_TYPE_RELATIONSHIPS_HPP
 
+#include <Tlpp/TypeTraits/basic_traits.hpp>
 #include <Tlpp/TypeTraits/integral_constant.hpp>
+#include <Tlpp/TypeTraits/voi>
 
 namespace tl
 {
@@ -22,6 +24,33 @@ namespace tl
 			template<typename T, typename... Ts>
 			inline constexpr bool is_any_of_v = (is_same_v<T, Ts> || ...);
 		}
+
+#if defined TLPP_MSVC || defined TLPP_GCC || defined TLPP_CLANG
+		template<typename Base, typename Derived>
+		inline constexpr bool is_base_of_v = __is_base_of(Base, Derived);
+#else
+		namespace detail
+		{
+			template<typename Base>
+			true_type test_base_of(const volatile Base*);
+			template<typename>
+			false_type test_base_of(const volatile void*);
+
+			template<typename, typename>
+			false_type is_base_of_impl(...);
+			template<typename Base, typename Derived>
+			decltype(test_base_of<Base>(static_cast<Derived*>(nullptr)))
+			is_base_of_impl(int);
+		} // namespace detail
+		template<typename Base, typename Derived>
+		inline constexpr bool is_base_of_v = is_class_v<Base> &&
+			is_class_v<Derived> && decltype(detail::is_base_of_impl<Base,
+		                                                            Derived>(
+				0))::value;
+#endif
+		template<typename Base, typename Derived>
+		struct is_base_of : bool_constant<is_base_of_v<Base, Derived>>
+		{};
 
 	} // namespace type_traits
 } // namespace tl
