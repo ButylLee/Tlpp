ed6b704f68a4307158d1036e6343f80138a1f21d
James Taylor <james.taylor_rw@outlook.com>
2022-03-08 13:02:25 +0800

    ...

--- include/Tlpp/TypeTraits/properties_traits.hpp
+++ include/Tlpp/TypeTraits/properties_traits.hpp
@@ -2,6 +2,8 @@
 #define TLPP_TYPE_TRAITS_PROPERTIES_TRAITS_HPP
 
 #include <Tlpp/Config.h>
+#include <Tlpp/TypeTraits/array_traits.hpp>
+#include <Tlpp/TypeTraits/composite_traits.hpp>
 #include <Tlpp/TypeTraits/operation_traits.hpp>
 
 namespace tl
@@ -18,6 +20,18 @@ namespace tl
 #endif
 		template<typename T>
 		struct is_trivial : bool_constant<is_trivial_v<T>>
+		{};
+
+#if defined TLPP_MSVC || defined TLPP_GCC || defined TLPP_CLANG
+		template<typename T>
+		inline constexpr bool is_standard_layout_v = __is_standard_layout(T);
+#else
+		template<typename T>
+		inline constexpr bool is_standard_layout_v =
+			is_scalar_v<remove_all_extents_t<T>>;
+#endif
+		template<typename T>
+		struct is_standard_layout : bool_constant<is_standard_layout_v<T>>
 		{};
 
 	} // namespace type_traits
