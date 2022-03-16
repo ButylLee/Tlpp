7cca380aee81b4c3e076101ab78d4d050e25c2f0
James Taylor <james.taylor_rw@outlook.com>
2022-03-17 00:33:11 +0800

    add Tlpp.hpp

---
+++ include/Tlpp/Tlpp.hpp
@@ -0,0 +1,12 @@
+﻿#ifndef TLPP_TLPP_HPP
+#define TLPP_TLPP_HPP
+
+#include <Console/Console.hpp>
+#include <Tlpp/Basic.hpp>
+#include <Tlpp/Config.h>
+#include <Tlpp/Exception.hpp>
+#include <Tlpp/Macro.h>
+#include <Tlpp/TypeTraits.hpp>
+#include <Tlpp/Utility.hpp>
+
+#endif // TLPP_TLPP_HPP
